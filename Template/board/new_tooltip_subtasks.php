b9187ebb55613e918ef26ddc49d7fcf708080400
creecros <creecros@gmail.com>
2017-11-02 00:53:46 -0400

    Create new_tooltip_subtasks.php

---
+++ Template/board/new_tooltip_subtasks.php
@@ -0,0 +1,26 @@
+<div class="tooltip-large">
+    <table class="table-small">
+        <tr>
+            <th class="column-70"><?= t('Subtask') ?></th>
+            <?= $this->hook->render('template:board:tooltip:subtasks:header:before-assignee') ?>
+            <th><?= t('Assignee') ?></th>
+        </tr>
+        <?php foreach ($subtasks as $subtask): ?>
+        <tr>
+            <?php if (! $this->projectRole->canCommentTask($task)): ?>
+            <td>
+                <?= $this->subtask->renderToggleStatus($task, $subtask) ?>
+            </td>
+            <?php endif ?>
+            <?= $this->hook->render('template:board:tooltip:subtasks:rows', array('subtask' => $subtask)) ?>
+            <td>
+                <?php if (! empty($subtask['username'])): ?>
+                    <?= $this->text->e($subtask['name'] ?: $subtask['username']) ?>
+                <?php else: ?>
+                    <?= t('Not assigned') ?>
+                <?php endif ?>
+            </td>
+        </tr>
+        <?php endforeach ?>
+    </table>
+</div>
