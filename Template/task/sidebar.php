b20f791df6b3d0fc1e7c9001b6a24aff5b250fc3
creecros <creecros@gmail.com>
2018-03-19 18:58:16 -0400

    Update sidebar.php

--- Template/task/sidebar.php
+++ Template/task/sidebar.php
@@ -1,5 +1,5 @@
 <?php if ($task['is_active'] == 1): ?>
         <li>
-            <?= $this->modal->small('comment-o', t('Add a comment'), 'CommentOnlyController', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
+            <?= $this->modal->small('comment-o', t('Add a comment'), 'CommentOnly', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
         </li>
 <?php endif ?>
