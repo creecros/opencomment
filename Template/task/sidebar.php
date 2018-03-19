e997375f795d68ab47736009c474ae1a2e267070
creecros <creecros@gmail.com>
2018-03-19 16:36:27 -0400

    Update sidebar.php

--- Template/task/sidebar.php
+++ Template/task/sidebar.php
@@ -1,4 +1,5 @@
-<?php if ($this->projectRole->canCommentTask($task)) == false : ?>
+<?php if ($this->projectRole->canCommentTask($task)): ?>
+<?php else: ?>
         <li>
             <?= $this->modal->small('comment-o', t('Add a comment'), 'CommentController', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
         </li>
