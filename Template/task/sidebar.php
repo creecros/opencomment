59a60876043a5d747a91e6bce393f90bfe23e239
creecros <creecros@gmail.com>
2018-03-19 21:23:42 -0400

    Update sidebar.php

--- Template/task/sidebar.php
+++ Template/task/sidebar.php
@@ -1,4 +1,4 @@
-<?php if ($task['is_active'] == 1): ?>
+<?php if ($this->projectUserRoleModel->getUserRole($task['project_id'], $this->request->getIntegerParam('user_id')) === Role::PROJECT_VIEWER): ?>
         <li>
             <?= $this->modal->small('comment-o', t('Add a comment'), 'CommentOnly', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
         </li>
